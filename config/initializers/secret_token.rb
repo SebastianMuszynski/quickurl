65061f9e96eb7038ff490044c92057034deed8a7
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2013-05-25 22:52:42 +0200

    The beginning of quickurl - rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickurl::Application.config.secret_token = 'd46716472ef311d87f852c6facb9ccfb711b85ccf40cc992a5b37a6c0600c73fc8e924c41b16cde65754f88425f208c7b2f3464e6306441a60a487fc06197885'
